dd044976cc1a30ed4791006b1652f9ef53f36f19
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-03 22:22:04 -0300

    Add files via upload

---
+++ Chapter 04/variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+	int x;
+	x = 3434;
+//	float if = 2.39
+	cout<<if;
+		
+}
---
+++ Chapter 05/operators.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+	int a = 10;
+	int b = 20;
+	cout<<(a-b);
+	cout<<endl;
+	cout<<(a+b);
+	cout<<endl;
+	cout<<(a/b);
+	cout<<endl;
+	cout<<(a*b);
+	cout<<endl;
+	cout<<(a%b);
+	cout<<endl;
+
+	cout<<"Comparision Operators";
+	cout<<endl;
+	cout<<(a>b);
+	cout<<endl;
+	cout<<(a<b);
+	cout<<endl;
+	cout<<(a==b);
+	cout<<endl;
+	cout<<(a!=b);
+	cout<<endl;
+	cout<<(a>=b);
+	cout<<endl;
+	cout<<(a<=b);
+
+  // and  or  not 
+	cout<<"Logical Operators";
+	cout<<endl;
+	cout<<(true && true);
+	cout<<endl; 
+	cout<<(true && false);
+	cout<<endl; 
+	cout<<(true || false); 
+	cout<<endl;
+	cout<<(!false);
+	cout<<endl;
+	cout<<(--a);
+	
+		
+}
