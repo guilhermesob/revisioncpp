385861ae49ffd806129d69935405775518eb564b
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-03 22:26:51 -0300

    Add files via upload

---
+++ data_types.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+	int x = 23;
+	float y = 2342.344;
+	cout<<x;
+	cout<<endl;
+	cout<<y;
+	
+	char ch = 'A';
+	string name = "Faisal Zamir";
+	cout<<name;
+
+	bool male = true;
+		
+}
